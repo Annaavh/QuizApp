084a1250254268ea4f9f20e44f5b6db6ff89d039
Anna <annaavetisyan175@gmail.com>
2023-12-12 21:49:35 +0400

    added types

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Difficulty, fetchQuizQuestions } from './API';
 import Card from './components/Card';
-import axios from "axios";
-import qa from "./db.json"
+import "./index.scss"
 
 
-const TOTAL_QUESTIONS = 10;
+
 
 const App = () => {
-  const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
-  const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
-  const [score, setScore] = useState(0);
-  const [gameOver, setGameOver] = useState(true);
-  console.log(qa)
 
-  // console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
-  useEffect(()=>{
-  //  fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY)
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple`);
-        const data = response.data.results;
-        console.log(data);
-      } catch (error) {
-        console.error("Error fetching quiz questions:", error);
-      }
-    };
-
-    fetchData();
-  },[])
-  
-
-  const startQuiz = async () => {
-
-  }
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-
-  }
-  const nextQuestion = () => {
-
-  }
   return (
     <div className="App">
-      <h1>Quiz</h1>
-      <button onClick={startQuiz}>Start</button>
-      <p>Score:</p>
-      <p>Loading Questions ...</p>
-      {/* <Card
-        questionNumber={number + 1}
-        total={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={userAnswers ? userAnswers[number] : undefined}
-        callback={checkAnswer}
-      /> */}
-      <button onClick={nextQuestion}>Next Question</button>
+      <Card />
     </div>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,31 +1,110 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import "./Card.scss";
+import data from "../db.json"
 
-type Props = {
-    question: string;
-    answers: string[];
-    callback: any;
-    userAnswer: any;
-    questionNumber: number;
-    total: number;
-}
 
-const Card: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNumber, total }) => {
-    return (
-        <div>
-            <p>Question: {questionNumber} / {total}</p>
-            <p dangerouslySetInnerHTML={{ __html: question }} ></p>
-            <div>
-                {
-                    answers.map(answer => (
-                        <div>
-                            <button disabled={userAnswer} onClick={callback}>
-                                <span dangerouslySetInnerHTML={{ __html: answer }} />
-                            </button>
-                        </div>
-                    ))
+const Card: React.FC = () => {
+
+    let [index, setIndex] = useState(0);
+    let [question, setQuestion] = useState(data[index])
+    let [lock, setLock] = useState(false)
+    let [score, setScore] = useState(0)
+    let [result, setResult] = useState(false)
+
+    let Option1 = useRef<HTMLLIElement | null>(null);
+    let Option2 = useRef<HTMLLIElement | null>(null);
+    let Option3 = useRef<HTMLLIElement | null>(null);
+    let Option4 = useRef<HTMLLIElement | null>(null);
+
+    let optionArr = [Option1, Option2, Option3, Option4]
+
+    const checkAnswer = (e: React.MouseEvent<HTMLLIElement>, answer: number) => {
+        if (lock === false) {
+            const target = e.target as HTMLElement | null;
+    
+            if (target && 'classList' in target) {
+                if (question.answer === answer) {
+                    target.classList.add("correct");
+                    setLock(true);
+                    setScore(prev => prev + 1);
+                } else {
+                    target.classList.add("wrong");
+                    setLock(true);
+                    optionArr[question.answer - 1]!.current!.classList.add("correct");
                 }
-            </div>
+            }
+        }
+    };
+    
+
+    const next = () => {
+        if (lock === true) {
+            if (index === data.length - 1) {
+                setResult(true);
+                return 0;
+            }
+            setIndex(++index);
+            setQuestion(data[index]);
+            setLock(false);
+            optionArr.map((option) => {
+                option!.current!.classList.remove("wrong");
+                option!.current!.classList.remove("correct");
+                return null;
+            })
+        }
+    }
+
+    const reset = () => {
+        setIndex(0);
+        setQuestion(data[0]);
+        setScore(0);
+        setLock(false)
+        setResult(false)
+    }
+    const prev = () => {
+        if (index > 0) {
+            setIndex(prevIndex => prevIndex - 1);
+            setQuestion(data[index - 1]);
+            setLock(false);
+            optionArr.forEach(option => {
+                if (option && option.current) {
+                    option.current.classList.remove("wrong");
+                    option.current.classList.remove("correct");
+                }
+            });
+        }
+
+    };
+    return (
+        <div className="container">
+            <h1>Quiz App</h1>
+            <hr />
+            {result ? <div className="container_result">
+                <h2>You Scored {score} out of {data.length} </h2>
+                <button onClick={() => reset()}>Reset</button>
+            </div> : <>
+                <h2>{index + 1}. {question.question}</h2>
+                <ul>
+                    <li ref={Option1} onClick={(e) => checkAnswer(e, 1)}>{question.option1}</li>
+                    <li ref={Option2} onClick={(e) => checkAnswer(e, 2)}>{question.option2}</li>
+                    <li ref={Option3} onClick={(e) => checkAnswer(e, 3)}>{question.option3}</li>
+                    <li ref={Option4} onClick={(e) => checkAnswer(e, 4)}>{question.option4}</li>
+                </ul>
+                <div className="container_btn">
+                    <button disabled={index <= 0}
+                        onClick={() => prev()}
+                        className={index <= 0 ? "prev" : ""}
+                    >Prev</button>
+                    <button onClick={() => next()}>Next</button>
+                </div>
+
+                <div className="index">{index + 1} of {data.length} questions</div>
+            </>}
+
         </div>
     )
 };
 export default Card;
+
+
+
